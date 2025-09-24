ce08074f806b77f09d7f61af2bb672717bc70f0e
Vinandydy <vinandy4718@gmail.com>
2025-09-24 10:50:37 +0300

    Mocks added

--- Parse/main/tests.py
+++ Parse/main/tests.py
@@ -1,27 +1,78 @@
 from django.test import TestCase
 from unittest import mock
-from main.serializers import MainPostSerializer
+from .services import partial
+from urllib.parse import urljoin
+from requests.exceptions import HTTPError
 
 
 class MainPostSerializerTest(TestCase):
 
-    @mock.patch('main.serializers.partial')
-    def test_create_new_bookmark_success(self, mock_partial):
-        mock_partial.return_value = {
-            'url': 'http://fdsfsd.com',
-            'name': 'trhrthtrhrt',
-            'favicon': 'http://fdsfsd.com/favicon.ico',
-            'description': 'trhyrtjuyh ythu ty hyrt hrth rthrt'
-        }
+    @mock.patch('main.services.requests')
+    def test_success_request(self, mock_get):
 
-        data = {'url': 'http://fdsfsd.com'}
-        serializer = MainPostSerializer(data=data)
-        self.assertTrue(serializer.is_valid())
-        bookmark = serializer.save()
+        data = '''
+        <html>
+            <head>
+                <title>Title</title>
+                <link rel="icon" href="/favicon.ico">
+                <meta name="description" content="Smth there">
+            </head>
+        </html>
+        '''
+        mock_response = mock.Mock()
+        mock_response.status_code = 200
+        mock_response.text = data
 
-        self.assertEqual(bookmark.url, 'http://fdsfsd.com')
-        self.assertEqual(bookmark.title, 'trhrthtrhrt')
-        self.assertEqual(bookmark.favicon, 'http://fdsfsd.com/favicon.ico')
-        self.assertEqual(bookmark.description, 'trhyrtjuyh ythu ty hyrt hrth rthrt')
+        mock_get.get.return_value = mock_response
+        url = 'http://test.com'
+        result = partial(url)
 
-        mock_partial.assert_called_once_with('http://fdsfsd.com')
+        favicon = urljoin(url, '/favicon.ico')
+        self.assertEqual(result, {
+            "url": url,
+            "name": "Title",
+            "favicon": favicon,
+            "description": "Smth there",
+        })
+        self.assertNotIn("error", result)
+
+    @mock.patch('main.services.requests')
+    def test_part_success_request(self, mock_get):
+        data = '''
+            <html>
+                <head>
+                    <link rel="icon" href="/favicon.ico">
+                </head>
+            </html>
+            '''
+        mock_response = mock.Mock()
+        mock_response.status_code = 200
+        mock_response.text = data
+
+        mock_get.get.return_value = mock_response
+        url = 'http://test.com'
+        result = partial(url)
+
+        favicon = urljoin(url, '/favicon.ico')
+        self.assertEqual(result, {
+            "url": url,
+            "name": "No title",
+            "favicon": favicon,
+            "description": "",
+        })
+        self.assertNotIn("error", result)
+
+    @mock.patch('main.services.requests')
+    def test_error_request(self, mock_get):
+        mock_response = mock.Mock()
+        mock_response.status_code = 404
+        mock_response.text = 'Not Found'
+        mock_response.raise_for_status.side_effect = HTTPError('404 Client Error: Not Found')
+        mock_get.get.return_value = mock_response
+
+        url = 'http://test.com'
+        result = partial(url)
+
+        self.assertIn('error', result)
+        self.assertEqual(result['url'], url)
+
